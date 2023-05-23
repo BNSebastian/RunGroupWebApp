1a4f9c12ec62ba8e321ed22fc9368bc79f77b612
Sebastian BN <bucovala.sebastian.nicu@gmail.com>
2023-05-23 21:35:40 +0300

    update

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
